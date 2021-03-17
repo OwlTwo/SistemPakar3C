fe0aec24104c4baabe2a8a9de48fd3b05e0b11b5
adityaluthfimaulanaharahap <aditluthfi21@gmail.com>
2021-03-17 23:28:40 +0700

    Push Adit

--- 1184090_Aditya Luthfi Maulana Harahap/BatuBata.py
+++ 1184090_Aditya Luthfi Maulana Harahap/BatuBata.py
@@ -1,19 +1,9 @@
-# kecerdasanbuatan
-Modul Praktikum Kecerdasan Buatan
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 12:27:28 2021
 
-Tugas 1
-
-Harga batu bata satuan buah
-1.600 (A) <= 650 (A).
-2.650 (A) <= mahal (A).
-3.650 (A), 700 (A) <= sangat mahal (A).
-4.650 ("mahal").
-5.700 ("mahal ").
-6.600 ("standar").
-
-Penjelasan kode python :
-
-1. Kode di bawah ini untuk mendefinisikan variable global dan juga menyimpan nya
+@author: Aditya Luthfi
+"""
 
 global facts
 global is_changed
@@ -22,8 +12,30 @@ print("Forward")
 is_changed = True
 facts = [["650","mahal"],["700","mahal"],["600","standar"]]
 
+def assert_fact(fact):
+    global facts
+    global is_changed
+    if not fact in facts:
+        facts += [fact]
+        is_changed = True
 
-2. Kode dibawah ini untuk menangkapa data data baru
+while is_changed:
+    is_changed = False
+    for A1 in facts:
+        if A1[0] == "600":
+            assert_fact(["650",A1[1]])
+        if A1[0] == "650":
+            assert_fact(["mahal",A1[1]])
+        if A1[0] == "650" and ["700",A1[1]] in facts:
+            assert_fact(["sangat mahal",A1[1]])
+
+
+print(facts)
+
+
+print("Backward")
+is_changed = True
+facts = [["650","mahal"],["700","mahal"],["600","standar"]]
 
 def assert_fact(fact):
     global facts
@@ -32,20 +44,6 @@ def assert_fact(fact):
         facts += [fact]
         is_changed = True
 
-3. Codingan forward fungsinya untuk melooping(perulangan) aturan yang di tetapkan metode forward
-
-while is_changed:
-    is_changed = False
-    for A1 in facts:
-        if A1[0] == "600":
-            assert_fact(["650",A1[1]])
-        if A1[0] == "650":
-            assert_fact(["mahal",A1[1]])
-        if A1[0] == "650" and ["700",A1[1]] in facts:
-            assert_fact(["sangat mahal",A1[1]])
-
-4. Codingan backward untuk melooping(perulangan) aturan  metode backward
-
 while is_changed:
     is_changed = False
     for A1 in facts:
@@ -55,4 +53,6 @@ while is_changed:
             assert_fact(["mahal",A1[1]])
         if A1[0] == "600":
             assert_fact(["650",A1[1]])
+        
 
+print(facts)
