5e8ad4e4673bd08f2c8bd2919378b3a730d76e1e
steward <izalr909@gmail.com>
2021-03-17 13:34:13 +0700

    1184033 Rizal Ramadhan Forward Backward

--- 1184033_Rizal Ramadhan/forwardbackward.py
+++ 1184033_Rizal Ramadhan/forwardbackward.py
@@ -1,18 +1,11 @@
-# kecerdasanbuatan
-Modul Praktikum Kecerdasan Buatan
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 12:27:28 2021
+
+@author: Rizal
+"""
 
 
-Tugas 1 Sistem Pakar
-
-Aturan tinggi badan
-1.150 (A) <= 180 (A).
-2.180 (A) <= tinggi (A).
-3.180 (A), 190 (A) <= sangat tinggi (A).
-4.180 ("tinggi").
-5.190 ("tinggi").
-6.150 ("pendek").
-
-kode di bawah ini untuk mendefinisikan variable global dan juga menyimpan nya
 
 global facts
 global is_changed
@@ -21,8 +14,30 @@ print("Forward")
 is_changed = True
 facts = [["180","tinggi"],["190","tinggi"],["150","pendek"]]
 
+def assert_fact(fact):
+    global facts
+    global is_changed
+    if not fact in facts:
+        facts += [fact]
+        is_changed = True
 
-dibawah ini untuk menangkapa data data baru
+while is_changed:
+    is_changed = False
+    for A1 in facts:
+        if A1[0] == "150":
+            assert_fact(["180",A1[1]])
+        if A1[0] == "180":
+            assert_fact(["tinggi",A1[1]])
+        if A1[0] == "180" and ["190",A1[1]] in facts:
+            assert_fact(["sangat tinggi",A1[1]])
+
+
+print(facts)
+
+
+print("Backward")
+is_changed = True
+facts = [["180","tinggi"],["190","tinggi"],["150","pendek"]]
 
 def assert_fact(fact):
     global facts
@@ -31,20 +46,6 @@ def assert_fact(fact):
         facts += [fact]
         is_changed = True
 
-dibawah ini adalah codingan forward fungsinya untuk melooping aturan yang di tetapkan metode forward
-
-while is_changed:
-    is_changed = False
-    for A1 in facts:
-        if A1[0] == "150":
-            assert_fact(["180",A1[1]])
-        if A1[0] == "180":
-            assert_fact(["tinggi",A1[1]])
-        if A1[0] == "180" and ["190",A1[1]] in facts:
-            assert_fact(["sangat tinggi",A1[1]])
-
-dibawah ini adalah codingan backward untuk melooping aturan  metode backward
-
 while is_changed:
     is_changed = False
     for A1 in facts:
@@ -54,3 +55,9 @@ while is_changed:
             assert_fact(["tinggi",A1[1]])
         if A1[0] == "150":
             assert_fact(["180",A1[1]])
+        
+
+print(facts)
+
+
+
